6c30c858b6cc48da328fabdb75d239c80a5af28c
JM <daga4242@gmail.com>
2022-02-22 19:09:44 +0900

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+    return(
+        <HeaderBox>
+            <h1><a href="/">잉스타</a></h1>
+        </HeaderBox>
+    )
+}
+
+const HeaderBox = styled.header`
+    background-color:tomato;
+    width:100%;
+    height:10vh;
+`
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   document.getElementById('root')
 );
